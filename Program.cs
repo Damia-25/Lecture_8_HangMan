476835478b2b4640f43d3464b98c2750b1213d63
Daniil <daniilmiadzvetski@gmail.com>
2025-10-01 10:09:13 +0200

    UPdate code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,56 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+// --init
+string secret="hydrochlorid";
+
+
+
+
+// --helper
+bool[] guesses = new bool['z'-'a']; // false -> has not been guessed
+
+
+
+int char2int (char c) {
+  return c-'a';
+}
+
+char int2char (int i) {
+  return (char)(((int)'a')+i);
+}
+
+void print_status () {
+  // guesses
+  Console.Write("Guesses: ");
+  for (int i=0 ; i<guesses.Length ; i++) {
+    if (guesses[i]) {
+      Console.Write(int2char(i));
+    }
+  }
+  Console.WriteLine("");
+}
+
+
+
+// --main
+
+
+while (true) {
+  print_status();
+  Console.WriteLine("Give us a guess: ");
+  string? input = Console.ReadLine();
+  if (input==null || input.Length!=1) {
+    Console.WriteLine("Error: Input should be 1 long");
+    continue;
+  }
+  char c = Char.ToLower(input[0]);
+
+   if (c<'a' || c>'z') {
+    Console.WriteLine("Only letters acceptable");
+    continue;
+  }
+
+  Console.WriteLine(c);
+  guesses[char2int(c)]=true;
+
+}
